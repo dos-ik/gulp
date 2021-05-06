c0e11bf57b9f892c3c81ad9a3b8df047ce3d5fad
kz <kz@email.com>
2021-05-06 16:01:35 +0900

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,43 @@
-/*====================================
-package
- ====================================*/
 const gulp = require('gulp');
-const sass = require('gulp-sass');  // scssコンパイル
-const postcss = require('gulp-postcss');  // postcss本体
-const autoprefixer = require('autoprefixer'); // postcssの一部
-const mqpacker = require('css-mqpacker'); // postcssの一部
-const cssdeclsort = require('css-declaration-sorter');  // postcssの一部(並び替え)
+
+// css
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssdeclsort = require('css-declaration-sorter');
+const gcmq = require('gulp-group-css-media-queries');
+
+// html
 const pug = require('gulp-pug');
-const plumber = require("gulp-plumber");  // 監視継続
-const notify = require('gulp-notify');  // 通知
-const imagemin = require("gulp-imagemin"); // 画像圧縮
+
+// image
+const imagemin = require("gulp-imagemin");
 const imageminJpg = require("imagemin-mozjpeg");
 const imageminPng = require("imagemin-pngquant");
 const imageminGif = require("imagemin-gifsicle");
 
-/*==================================
- config
- ==================================*/
-// path 設定
+// utility
+const notify = require('gulp-notify');	// 通知
+const plumber = require("gulp-plumber");	// 監視継続
+
+
+// path
 var rootdir = './dest';
 var destdir = rootdir;
 var srcdir = {
-  scss: './src/**/*.scss',
+  scss: ['./src/**/*.scss'],
   pug: ['./src/**/*.pug', '!src/**/_*.pug', '!./src/_*/**/*'],
   image: './src/**/*.+(jpg|jpeg|png|gif)',
   copy: ['./src/**/*', '!./src/**/*.+(pug|scss|jpg|jpeg|png|gif)']
 }
 
 
-/*==================================
-task
-==================================*/
+//
+// task
+//
 
-// sass コンパイル
+// sass
 function styles() {
-
   // post-css プラグインの設定
   var plugins = [
     cssdeclsort({
@@ -45,26 +47,26 @@ function styles() {
     autoprefixer({
       // IEは11以上、Androidは4.4以上
       // その他は最新2バージョンで必要なベンダープレフィックスを付与する設定
-      browsers: ['ie >= 10', 'Android >= 4'],
+      browsers: ["last 2 versions", "ie >= 11", "Android >= 4"],
       cascade: false
-    }),
-    // media-query をまとめる
-    mqpacker()
+    })
   ];
 
-  return gulp.src(srcdir.scss)
-    // return gulp.src(srcdir.scss, { sourcemaps: true })
-    .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))  // watch エラー監視継続+DP通知
+  return gulp
+    .src(srcdir.scss, { sourcemaps: true, })
+    // watchエラー監視継続+DP通知
+    .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
     .pipe(sass({ outputStyle: 'expanded' }))
-    .pipe(postcss(plugins)) // 設定したpost-cssを実行
-    .pipe(gulp.dest(destdir))
-  // .pipe(gulp.dest(destdir,{ sourcemaps: './' }))
+    .pipe(postcss(plugins))	// 設定したpost-cssを実行
+    .pipe(gcmq())	// メディアクエリをまとめる
+    .pipe(gulp.dest(destdir, { sourcemaps: './' }))	// ソースマップを生成してコンパイル
 }
 
-// pug コンパイル
+// pug
 function html() {
-  return gulp.src(srcdir.pug)
-    .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))  // watch エラー監視継続+DP通知
+  return gulp
+    .src(srcdir.pug)
+    .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
     .pipe(pug({ pretty: true }))
     .pipe(gulp.dest(destdir))
 
@@ -75,10 +77,10 @@ function html() {
    */
 }
 
-
 // 画像圧縮
 function images() {
-  return gulp.src(srcdir.image, { since: gulp.lastRun(images) })
+  return gulp
+    .src(srcdir.image, { since: gulp.lastRun(images) })
     .pipe(imagemin([
       imageminPng(),
       imageminJpg({
@@ -95,12 +97,15 @@ function images() {
     .pipe(gulp.dest(destdir))
 }
 
+
 // ファイルコピー
 function copy() {
-  return gulp.src(srcdir.copy)
+  return gulp
+    .src(srcdir.copy)
     .pipe(gulp.dest(destdir));
 }
 
+// 監視
 function watch() {
   gulp.watch(srcdir.scss, styles);
   gulp.watch(srcdir.pug, html);
@@ -108,6 +113,7 @@ function watch() {
   gulp.watch(srcdir.image, images);
 }
 
+// 実行コマンド
 exports.build = gulp.parallel(styles, html, images, copy);
 exports.default = watch;
 exports.watch = watch;
@@ -116,3 +122,4 @@ exports.html = html;
 exports.images = images;
 exports.copy = copy;
 
+ // npm i -D gulp-sass gulp-postcss autoprefixer css-declaration-sorter gulp-group-css-media-queries gulp-pug gulp-plumber gulp-notify gulp-imagemin imagemin-mozjpeg imagemin-pngquant imagemin-gifsicle
